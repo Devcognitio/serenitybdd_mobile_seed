a8f3781204e5ca7c8bcf729ce38eddf479a8642c
guseche <guseche@bancoagricola.com.sv>
2022-10-27 08:35:11 -0500

    Ajuste en versiones de dependencias y .gitignore

--- src/main/java/co/com/devco/tasks/Login.java
+++ src/main/java/co/com/devco/tasks/Login.java
@@ -1,6 +1,9 @@
 package co.com.devco.tasks;
 
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.waits.WaitUntil;
@@ -10,7 +13,7 @@ import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isEnab
 
 public class Login{
 
-    public static Task atSwagLabs() {
+    public static Performable atSwagLabs() {
         return Task.where(
                 WaitUntil.the(TXT_USERNAME,isEnabled()).forNoMoreThan(5).seconds(),
                 Enter.theValue("standard_user").into(TXT_USERNAME),
--- src/test/java/co/com/devco/runners/BuyingProductsRunner.java
+++ src/test/java/co/com/devco/runners/BuyingProductsRunner.java
@@ -4,6 +4,7 @@ import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
+
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features/buying_products.feature",
